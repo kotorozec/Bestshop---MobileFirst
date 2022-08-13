692d9a434ba26415ecaf35b9c3f789726119479a
Dominik <dominik.kantorowicz@gmail.com>
2022-08-13 15:15:38 +0200

    header with responsive menu

---
+++ dist/index.72be8890.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".header__menu__hamburger");
+const menu = document.querySelector(".header__menu");
+const links = document.querySelector(".header__menu__nav-item__link");
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    links.classList.toggle("active");
+});
+document.querySelectorAll(".header__menu__nav-item__link").forEach((n)=>n.addEventListener("click", ()=>{
+        hamburger.classList.remove("active");
+        menu.classList.remove("active");
+        links.classList.remove("active");
+    }));
+
+//# sourceMappingURL=index.72be8890.js.map
